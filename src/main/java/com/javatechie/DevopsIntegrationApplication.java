f75d77edb443ed0c0f327d4594750767c14de568
adarsha14 <111411927+adarsha14@users.noreply.github.com>
2022-08-23 21:25:51 +0530

    Update DevopsIntegrationApplication.java

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie";
+		return "welcome, this is team10";
 	}
 
 	public static void main(String[] args) {
